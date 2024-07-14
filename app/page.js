83a1f18d99170ace2ee41772d0a859af24c7c300
joanescolanuria <joanescolanuria@gmail.com>
2024-07-14 17:26:58 +0200

    tailwind

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Header from "@/Components/Header";
 import BlogList from "@/Components/BlogList";
 import Footer from "@/Components/Footer";
+import '../styles/globals.css';
 
 export default function Home() {
   return (
